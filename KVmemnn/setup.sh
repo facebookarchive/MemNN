85ee517d4117fc656b91be53ee0c0a13c27e7645
Alexander Miller <ahm@fb.com>
2017-04-04 15:08:56 -0400

    updated setup.sh

--- KVmemnn/setup.sh
+++ KVmemnn/setup.sh
@@ -1,8 +1,40 @@
 #!/bin/bash
 # Copyright 2004-present Facebook. All Rights Reserved.
 
+luarocks show lzmq 1>/dev/null 2>/dev/null
+if [ $? -ne 0 ]; then
+    luarocks install lzmq
+    if [ $? -ne 0 ]; then
+        echo "Error: resolve then run luarocks install lzmq"
+        exit 1
+    fi
+fi
+
+luarocks show threads 1>/dev/null 2>/dev/null
+if [ $? -ne 0 ]; then
+    luarocks install threads
+    if [ $? -ne 0 ]; then
+        echo "Error: resolve then run luarocks install threads"
+        exit 1
+    fi
+fi
+
 # compile libmemnn.so
 gcc -O3 -std=c99 -shared -o ./library/c/libmemnn.so -fPIC ./library/c/*.c
+if [ $? -ne 0 ]; then
+    echo "Error compiling memnn library"
+    exit 1
+fi
+
 # link libmemnn.so to lua lib
 s=$(which luarocks)
-ln -s $(pwd)/library/c/libmemnn.so ${s%luarocks}/../lib/libmemnn.so
+syml_path="${s%luarocks}/../lib/libmemnn.so"
+if [ -e $syml_path -o -L $syml_path ]; then
+    rm $syml_path
+fi
+
+ln -s $(pwd)/library/c/libmemnn.so $syml_path
+if [ $? -ne 0 ]; then
+    echo "Error linking memnn library to luarocks lib directory"
+    exit 1
+fi
