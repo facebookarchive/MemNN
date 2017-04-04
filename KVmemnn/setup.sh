ef90e6a9a64a654e4aebee0869ef008eebd59b67
Alexander Miller <ahm@fb.com>
2017-04-04 15:08:56 -0400

    updated setup.sh

--- KVmemnn/setup.sh
+++ KVmemnn/setup.sh
@@ -5,4 +5,7 @@
 gcc -O3 -std=c99 -shared -o ./library/c/libmemnn.so -fPIC ./library/c/*.c
 # link libmemnn.so to lua lib
 s=$(which luarocks)
+if [ -e ${s%luarocks}/../lib/libmemnn.so ]; then
+    rm ${s%luarocks}/../lib/libmemnn.so
+fi
 ln -s $(pwd)/library/c/libmemnn.so ${s%luarocks}/../lib/libmemnn.so
