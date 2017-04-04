054e0e3a0fd74f0671c20c1cfcaeb6df0aee18e7
Alexander Miller <ahm@fb.com>
2017-04-04 15:48:52 -0400

    setup updates

--- KVmemnn/setup.sh
+++ KVmemnn/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Copyright 2004-present Facebook. All Rights Reserved.
 
+luarocks install lzmq threads
+
 # compile libmemnn.so
 gcc -O3 -std=c99 -shared -o ./library/c/libmemnn.so -fPIC ./library/c/*.c
 # link libmemnn.so to lua lib
