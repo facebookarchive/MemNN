2f586da537ed5d149143d39387f143f414b050aa
Alexander Holden Miller <ahm@fb.com>
2019-10-23 11:07:07 +0100

    Delete setup_processed_data.sh

--- KVmemnn/examples/WikiMovies/setup_processed_data.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Copyright 2004-present Facebook. All Rights Reserved.
-
-wget "https://s3.amazonaws.com/fair-data/memnn/kvmemnn/data.tar.gz" \
-&& tar -xzvf data.tar.gz && rm data.tar.gz
