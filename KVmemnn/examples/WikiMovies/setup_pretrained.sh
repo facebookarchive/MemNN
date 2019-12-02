15296d7b88a91e039fe5dca78dca04c62417cc8b
Alexander Holden Miller <ahm@fb.com>
2019-12-02 17:52:03 +0000

    Delete setup_pretrained.sh

--- KVmemnn/examples/WikiMovies/setup_pretrained.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Copyright 2004-present Facebook. All Rights Reserved.
-
-wget "https://s3.amazonaws.com/fair-data/memnn/kvmemnn/output.tar.gz" \
-&& tar -xzvf output.tar.gz && rm output.tar.gz
